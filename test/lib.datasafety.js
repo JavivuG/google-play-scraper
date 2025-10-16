3ebf500323e15e6a31b4f9a8e1ee0a5b09b702e1
Javier <javiergarciaglz16@gmail.com>
2025-10-16 17:16:35 +0000

    feat(datasafety): fallback mappings

--- test/lib.datasafety.js
+++ test/lib.datasafety.js
@@ -2,18 +2,26 @@ import gplay from '../index.js';
 import { assert } from 'chai';
 import { assertValidUrl } from './common.js';
 
+function assertValidOptional(value) {
+  assert(
+    typeof value === 'boolean' || value === 0 || value === 1,
+    'optional must be boolean or 0/1'
+  );
+}
+
+
 function assertValidDataSafetyObject () {
   return (entry) => {
     assert.isString(entry.data);
     assert.isString(entry.purpose);
     assert.isString(entry.type);
-    assert.isBoolean(entry.optional);
+    assertValidOptional(entry.optional);
   };
 }
 
 describe('Data Safety method', () => {
   it('should return arrays of data shared, data collected, security practices and a privacy url', () =>
-    gplay.datasafety({ appId: 'com.sgn.pandapop.gp' })
+    gplay.datasafety({ appId: 'com.zhiliaoapp.musically' })
       .then((dataSafety) => {
         assert.isArray(dataSafety.sharedData);
         assert.isArray(dataSafety.collectedData);
@@ -22,14 +30,14 @@ describe('Data Safety method', () => {
       }));
 
   it('should return a valid shared and collected data object', () =>
-    gplay.datasafety({ appId: 'com.sgn.pandapop.gp' })
+    gplay.datasafety({ appId: 'com.zhiliaoapp.musically' })
       .then((dataSafety) => {
         dataSafety.sharedData.map(assertValidDataSafetyObject());
         dataSafety.collectedData.map(assertValidDataSafetyObject());
       }));
 
   it('should return a valid security practices object', () =>
-    gplay.datasafety({ appId: 'com.sgn.pandapop.gp' })
+    gplay.datasafety({ appId: 'com.zhiliaoapp.musically' })
       .then((dataSafety) => {
         dataSafety.securityPractices.forEach((practice) => {
           assert.isString(practice.practice);
